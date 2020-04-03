b7e5e3739f566e36263bde7fcd527b9724118aa8
tgadeliya <tgadeliya@gmail.com>
2020-04-03 16:49:54 +0200

    add post request

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
+from typing import Dict
 from fastapi import FastAPI
 from pydantic import BaseModel
+
+
 app = FastAPI()
+app.counter = 0
+
+
+class HelloResp(BaseModel):
+    msg: str
 
 
 
-class 
+@app.get("/counter")
+def counter():
+    app.counter += 1
+    return str(app.counter)
 
 @app.get("/")
 def root():
     return {"message": "Hello Wow!"}
 
-@app.get("/hello/{name}")
+@app.get("/hello/{name}", response_model=HelloResp)
 async def read_name(name:str):
-    return f"Hello {name}"
+    return HelloResp(msg=f"Hello {name}")
+
+
+
+class GiveMeSomethingRq(BaseModel):
+    name:str
+
+
+class GiveMeSomethingResp(BaseModel):
+    received: Dict
+    constant_data: str = "Jan Nowak"
+
+
+@app.post("/db", response_model=GiveMeSomethingResp)
+def receive_name(rq: GiveMeSomethingRq):
+    return GiveMeSomethingResp(received=rq.dict())
