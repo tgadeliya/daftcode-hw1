5f6fc8b35bd2873d31e66e4327b4c76d02b84253
tgadeliya <tgadeliya@gmail.com>
2020-04-06 10:56:02 +0200

    Task2 finished

--- main.py
+++ main.py
@@ -11,24 +11,49 @@ class HelloResp(BaseModel):
     msg: str
 
 
-
 @app.get("/counter")
 def counter():
     app.counter += 1
     return str(app.counter)
 
+
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
+
 @app.get("/hello/{name}", response_model=HelloResp)
-async def read_name(name:str):
+async def read_name(name: str):
     return HelloResp(msg=f"Hello {name}")
 
 
+@app.get("/method")
+def method_get():
+    return {"method": "GET"}
+
+
+@app.post("/method")
+def method_post():
+    return {"method": "POST"}
+
+
+@app.delete("/method")
+def method_delete():
+    return {"method": "DELETE"}
+
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
+
+# TODO: Check whether possible to __meta__ parse request to method
+# @getattr(app, "get")("/")
+# def resp():
+# return 1
+
 
 class GiveMeSomethingRq(BaseModel):
-    name:str
+    name: str
 
 
 class GiveMeSomethingResp(BaseModel):
