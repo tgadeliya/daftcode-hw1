ee3934cac4ea05d8ee588a238f3737f34f515c6d
tgadeliya <tgadeliya@gmail.com>
2020-04-25 16:49:39 +0200

    w3a2

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+import secrets
+from hashlib import sha256
 from typing import Dict
-from fastapi import FastAPI, Response, status, HTTPException
-from pydantic import BaseModel
 
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
-
+app.secret_key = "There are places I'll remember All my life, though some have changed Some forever, not for better Some have gone, and some remain"
 app.last_patient_num = -1
 app.patient_db = dict()
 
 
 @app.get("/")
 def root():
-    return {"message": "Hello, hello \\n I don't know why you say goodbye, I say hello".replace("\\n", "\n")}
+    return {"message": "Hello, hello. I don't know why you say goodbye, I say hello"}
 
 
 @app.get("/welcome")
 def welcome_msg():
-    return {"message": "Hello, hello \\n I don't know why you say goodbye, I say hello".replace("\\n", "\n")}
+    return {"message": "Hello, hello. I don't know why you say goodbye, I say hello"}
 
 # TODO: Check whether possible to __meta__ parse request to method
 @app.get("/method")
@@ -58,3 +63,19 @@ def read_patient(pk: int):
         if pk not in app.patient_db:
             raise  HTTPException(status_code=204)
         return app.patient_db[pk]
+
+
+@app.post("/login/")
+def session_login_with_cookies(user: str, password: str, respone: Response):
+    
+    correct_username = secrets.compare_digest(user, "trudnY")
+    correct_password = secrets.compare_digest(password, "PaC13Nt")
+    
+    if not(correct_username and correct_password):
+        raise HTTPException(status_code = 401)
+    
+    session_token = sha256(bytes(f"{user}{password}{app.secret_key}").hexdigest())
+    respone.set_cookie(key = "session_token", value = session_token)
+    
+    return RedirectResponse("/welcome")
+    
