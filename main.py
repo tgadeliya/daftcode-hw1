4fa66f6fd3682700224c69d0c24c04d4a281e789
tgadeliya <tgadeliya@gmail.com>
2020-05-01 17:02:18 +0200

    added db and tracks/

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from typing import Dict
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
+import sqlite3
+
 from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 
@@ -33,11 +35,13 @@ class Patient_response(BaseModel):
     id: int
     patient: Dict
 
+
 @app.post("/patient", response_model=Patient_response)
 def patient(rq: Patient_request):
     app.last_patient_num += 1
     app.patient_db[app.last_patient_num] = rq.dict()
     return Patient_response(id=app.last_patient_num, patient=rq)
+
 
 @app.get("/patient/{pk}")
 def read_patient(pk: int):
@@ -59,3 +63,26 @@ def session_login_with_cookies(user: str, password: str, response: Response):
         response.set_cookie(key="session_token", value=session_token)
 
         return RedirectResponse(url="https://daftcode-hw1.herokuapp.com/welcome", status_code=302)
+
+
+@app.get(f"/tracks")
+async def tracks(page: int, per_page: int):
+    with sqlite3.connect("chinook.db") as connection:
+        conn = connection.cursor()
+        conn.row_factory = sqlite3.Row
+        tracks = conn.execute(f"SELECT *\
+                               FROM Tracks\
+                               ORDER BY TrackId\
+                               LIMIT {per_page} OFFSET {per_page*page}").fetchall()
+
+        return {
+            "TrackId": tracks["TrackId"],
+            "Name": tracks["Name"],
+            "AlbumId": tracks["AlbumId"],
+            "MediaTypeId": tracks["MediaTypeId"],
+            "GenreId": tracks["GenreId"],
+            "Composer": tracks["Composer"],
+            "Milliseconds": tracks["Milliseconds"],
+            "Bytes": tracks["Bytes"],
+            "UnitPrice": tracks["UnitPrice"]
+        }
